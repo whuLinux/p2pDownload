7023723d1bf434e64b41d29d74ed87f05fe73d97
Yu-Jiale <yujiale.whu@gmail.com>
2019-11-09 17:43:24 +0800

    HTTP模块：实现断点续传

--- http.cpp
+++ http.cpp
@@ -7,7 +7,9 @@
 #include <QFileInfo>
 #include <QFileDialog>
 
-Http::Http(QWidget *parent) : QWidget(parent), ui(new Ui::Http) {
+Http::Http(QWidget *parent)
+        : QWidget(parent), ui(new Ui::Http),
+          totalBytes(0), bytesRead(0), bytesLastRead(0) {
 
     ui->setupUi(this);
     ui->progressBar->hide();
@@ -21,63 +23,97 @@ Http::~Http() {
 
     delete manager;
     delete reply;
+
     delete file;
 }
 
-void Http::startRequest(QUrl url) {
+void Http::startRequest(QUrl url, qint64 bytesLastRead) {
 
-    /* 获取请求的响应 */
-    reply = manager->get(QNetworkRequest(url));
+    /* 从bytesRead 处开始继续传输 */
+    qDebug() << "bytesLastRead = " << bytesLastRead;
+    QString range = QString("bytes=%1-").arg(bytesLastRead);
 
-    /* 关联槽函数：开始下载、更新下载进程、结束下载 */
+    /* 创建请求 */
+    QNetworkRequest header;
+    header.setUrl(url);
+    header.setRawHeader("Range", range.toLatin1());
+
+    /* 获取响应 */
+    reply = manager->get(QNetworkRequest(header));
+
+    /* 关联槽函数：开始下载、更新下载进程、结束下载、下载错误 */
     connect(reply, SIGNAL(readyRead()),
             this,  SLOT  (httpReadyRead()));
     connect(reply, SIGNAL(downloadProgress(qint64, qint64)),
             this,  SLOT  (httpDownloadProgress(qint64, qint64)));
     connect(reply, SIGNAL(finished()),
             this,  SLOT  (httpFinished()));
+    connect(reply, SIGNAL(error(QNetworkReply::NetworkError)),
+            this,  SLOT  (httpError(QNetworkReply::NetworkError)));
 }
 
 void Http::httpReadyRead() {
-
     if (file) {
-        /* 将请求到的响应全部写入文件 */
-        qDebug() << "开始下载";
         file->write(reply->readAll());
-    } else {
-        qDebug() << "打开文件失败";
     }
 }
 
 void Http::httpDownloadProgress(qint64 bytesRead, qint64 totalBytes) {
 
-    ui->progressBar->setMaximum(totalBytes);
-    ui->progressBar->setValue(bytesRead);
+    /* 更新数据 */
+    this->bytesRead = bytesRead;
+    this->totalBytes = totalBytes;
+
+    /* 更新进度条 */
+    ui->progressBar->setMaximum(totalBytes + bytesLastRead);
+    ui->progressBar->setValue(bytesRead + bytesLastRead);
 }
 
 void Http::httpFinished() {
     /* 响应结束后销毁相关对象 */
-    qDebug() << "下载完毕";
+    qDebug() << "下载结束";
+
+    int statusCode = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
+    qDebug() << "statusCode = " << statusCode;
+
+    if (statusCode >= 200 && statusCode < 400) {
+        qDebug() << "下载成功";
+        ui->pushButton_Begin->setEnabled(true);
+        ui->pushButton_Pause->setEnabled(false);
+    } else {
+        qDebug() << "下载失败";
+    }
+
     file->flush();
     file->close();
+    delete file;
+    file = nullptr;
 
     reply->deleteLater();
     reply = nullptr;
 
-    delete file;
-    file = nullptr;
+    totalBytes    = 0;
+    bytesRead     = 0;
+    bytesLastRead = 0;
 
     ui->progressBar->hide();
 }
 
+void Http::httpError(QNetworkReply::NetworkError error) {
+
+    qDebug() << "Error: " << error;
+}
+
 void Http::on_pushButton_Begin_clicked() {
     /* 点击开始下载按钮后执行请求 */
-    /* 首先读取 URL 并解析 */
+    ui->pushButton_Begin->setEnabled(false);
+
+    /* 首先读取 URL 并解析出文件名 */
     url = ui->lineEdit_Url->text();
-    qDebug() << "\nurl = " << url;
+    qDebug() << "url = " << url;
     QFileInfo info(url.path());
     qDebug() << "url.path = " << url.path();
-    QString fileName(info.fileName());
+    fileName = QString(info.fileName());
     qDebug() << "fileName = " << fileName;
 
     if (fileName.isEmpty()) {
@@ -85,32 +121,75 @@ void Http::on_pushButton_Begin_clicked() {
         qDebug() << "文件名为空，缺省为 index.html";
     }
 
-    /* 新建或打开文件，默认存放于可执行文件所在的路径 */
-    file = new QFile(path + fileName);
-    if (!file->open(QIODevice::WriteOnly)) {
+    /* 若文件存在且已下载字节数为 0 则说明下载未开始或重新下载 */
+    /* 此时需要把之前存在的临时文件删除 */
+//    if (bytesLastRead <= 0) {
+//        QFileInfo fileInfo(fileName);
+//        if (fileInfo.exists()) {
+//            QFile::remove(fileName);
+//        }
+//    }
+
+    /* 关闭上一次打开的文件 */
+    if (file) { delete file; }
+
+    /* 打开文件 */
+    file = new QFile(filePath + fileName);
+    if (!file->open(QIODevice::WriteOnly | QIODevice::Append)) {
         qDebug() << "打开文件失败";
         delete file;
         file = nullptr;
         return;
     }
 
-    qDebug() << "开始请求";
-    startRequest(url);
+    qDebug() << "开始下载";
+    bytesRead = 0;
+    totalBytes = 0;
 
-    ui->progressBar->setValue(0);
+    startRequest(url, bytesLastRead);
+
+    ui->pushButton_Pause->setEnabled(true);
+    ui->progressBar->setValue(bytesLastRead);
     ui->progressBar->show();
 }
 
 void Http::on_pushButton_Select_clicked() {
 
-    path = QFileDialog::getExistingDirectory(this, "选择下载路径", "/");
+    filePath = QFileDialog::getExistingDirectory(this, "选择下载路径", "/");
 
-    if (path.isEmpty()) {
+    if (filePath.isEmpty()) {
         qDebug() << "路径名为空，缺省为可执行文件所在路径";
     } else {
-        path += "/";
-        qDebug() << "path = " << path;
+        filePath += "/";
+        qDebug() << "path = " << filePath;
     }
 
-    ui->lineEdit_Path->setText(path);
+    ui->lineEdit_Path->setText(filePath);
+}
+
+void Http::on_pushButton_Pause_clicked() {
+
+    ui->pushButton_Pause->setEnabled(false);
+
+    bytesLastRead = file->size();
+
+    if (reply) {
+        /* 取消槽函数关联 */
+        disconnect(reply, SIGNAL(readyRead()),
+                   this,  SLOT  (httpReadyRead()));
+        disconnect(reply, SIGNAL(downloadProgress(qint64, qint64)),
+                   this,  SLOT  (httpDownloadProgress(qint64, qint64)));
+        disconnect(reply, SIGNAL(finished()),
+                   this,  SLOT  (httpFinished()));
+        disconnect(reply, SIGNAL(error(QNetworkReply::NetworkError)),
+                   this,  SLOT  (httpError(QNetworkReply::NetworkError)));
+
+        reply->abort();
+        reply->deleteLater();
+        reply = nullptr;
+    }
+
+    ui->pushButton_Begin->setEnabled(true);
+    ui->progressBar->setValue(bytesLastRead);
+    ui->progressBar->show();
 }
--- http.h
+++ http.h
@@ -8,6 +8,7 @@
 
 #include <QUrl>
 #include <QFile>
+#include <QTime>
 
 QT_BEGIN_NAMESPACE
     namespace Ui {
@@ -23,25 +24,36 @@ public:
     Http(QWidget *parent = nullptr);
     ~Http();
 
-    void startRequest(QUrl url);
+    void startRequest(QUrl url, qint64 bytesRead);
+
+    QUrl getUrl() { return url; }
 
 private slots:
     void httpReadyRead();
     void httpDownloadProgress(qint64 bytesRead, qint64 totalBytes);
     void httpFinished();
+    void httpError(QNetworkReply::NetworkError error);
 
     void on_pushButton_Begin_clicked();
 
     void on_pushButton_Select_clicked();
 
+    void on_pushButton_Pause_clicked();
+
 private:
-    Ui::Http *ui;
+    Ui::Http                *ui;
 
     QNetworkAccessManager   *manager;
     QNetworkReply           *reply;
-    QFile                   *file;
     QUrl                     url;
-    QString                  path;
+
+    QFile                   *file;
+    QString                  fileName;
+    QString                  filePath;
+
+    qint64                   totalBytes;        // 本次下载过程总共需要下载的字节数
+    qint64                   bytesRead;         // 本次下载过程中已下载的字节数
+    qint64                   bytesLastRead;     // 上一次断点处已下载的字节数
 };
 
 #endif // HTTP_H
